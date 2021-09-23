e9b0ecde3ffb9deafa6cfd0b369f016a929afb4a
Viktar-Daniil <gruzd.412@yandex.by>
2021-09-23 03:35:05 +0300

    asyncstorage is READY!!!

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { Provider } from "react-redux";
-import store from "./src/store/store";
+import { store, persistor } from "./src/store/store";
 import Navigator from "./src/navigator";
 import { SafeAreaView } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { PersistGate } from "redux-persist/integration/react";
 
 class App extends React.Component {
     render() {
         return (
             <Provider store={store}>
-                <SafeAreaView style={{ flex: 1 }}>
-                    <StatusBar />
-                    <Navigator />
-                </SafeAreaView>
+                <PersistGate persistor={persistor} loading={null}>
+                    <SafeAreaView style={{ flex: 1 }}>
+                        <StatusBar />
+                        <Navigator />
+                    </SafeAreaView>
+                </PersistGate>
             </Provider>
         );
     }
--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,23 @@ import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { rootReducer } from "./reducers";
 import rootSaga from "./sagas/rootSaga";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistReducer, persistStore } from "redux-persist";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
-sagaMiddleware.run(rootSaga);
+const persistConfig = {
+    key: "root",
+    storage: AsyncStorage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+    persistedReducer,
+    applyMiddleware(sagaMiddleware)
+);
+
+export const persistor = persistStore(store);
+
+sagaMiddleware.run(rootSaga);
